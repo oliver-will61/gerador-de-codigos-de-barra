982ac3a7e2cba0cde721333811df6f73f95e0afd
Willian de Oliveira Ribeiro <123466324+oliver-will61@users.noreply.github.com>
2023-07-17 11:51:41 -0300

    correção de um bug na div container-codigos

--- main.js
+++ main.js
@@ -1,41 +1,52 @@
 const entrada = document.querySelector("#entrada");
 const botao = document.querySelector("#botao");
+const containerCodigos = document.querySelector('#container-codigos');
 
-const codigoDoLote = 10000 + Math.round(Math.random()*8999)+"";
-let codigoDebarra = geraCodigoDeBarra();
+const valorMinimoPermitido = 1;
+const valorMaximoPermitido = 9999;
+const codigoDoLote = 100000 + Math.round(Math.random() * 89999) + "";
+
+let codigoDeBarra = geraCodigoDeBarra();
+let listaCodigos = [];
+
 
 botao.addEventListener("click", () => {
+  const quantidadeDeCodigos = entrada.value;
 
-    let quantidadeDeCodigos = entrada.value;
+  if (verificaSeENumero(quantidadeDeCodigos)) {
+    alert("Valor inválido. Por favor digite um número");
+  } else {
 
-    const valorMinimo = 1;
-    const valorMaximo = 9999;
-
-    if(quantidadeDeCodigos > valorMaximo || quantidadeDeCodigos < valorMinimo) {
-        alert (`Valor não permitido! O valor tem que estar entre ${valorMinimo} e ${valorMaximo}`);
-        entrada.value = 1;
+    if (verificaNumeroMinEMax(quantidadeDeCodigos)) {
+      alert(`Valor não permitido! O valor tem que estar entre ${valorMinimoPermitido} e ${valorMaximoPermitido}`);
     } else {
-        for (let i=1; i<= quantidadeDeCodigos; i++){
-            criaParagrafo();
-            codigoDebarra = parseInt(codigoDebarra) + 1;
-        }    
+      containerCodigos.classList.add("container-codigos");
+      for (let i = 1; i <= quantidadeDeCodigos; i++) {
+        criaParagrafo();
+        listaCodigos.push(codigoDeBarra);
+        codigoDeBarra = parseInt(codigoDeBarra) + 1;
+      }
     }
-})
+  }
+  entrada.value = "";
+});
 
-function criaParagrafo(){
-
-    const containerCodigos = document.querySelector('#container-codigos'); 
-
-    const paragrafo = document.createElement("p");
-    paragrafo.classList.add("paragrafo");
-    containerCodigos.appendChild(paragrafo);
-    
-    paragrafo.innerHTML = codigoDebarra + "0" 
+function criaContainerCodigos(){
+  const containerCodigos =  document.createElement("div");
+  containerCodigos.classList.add("container-codigos");
+  body.appendChild(containerCodigos);
 }
 
-function geraCodigoDeBarra(){
-    const codigoNacional = "789";
-    let codigoDaQuantidade = "0001";
-    return codigoNacional + codigoDoLote + codigoDaQuantidade;
+function criaParagrafo() {
+  const paragrafo = document.createElement("p");
+  paragrafo.classList.add("paragrafo");
+  containerCodigos.appendChild(paragrafo);
+
+  paragrafo.innerHTML = codigoDeBarra;
 }
 
+function geraCodigoDeBarra() {
+  const codigoNacional = "789";
+  let codigoDaQuantidade = "0001";
+  return codigoNacional + codigoDoLote + codigoDaQuantidade;
+}
